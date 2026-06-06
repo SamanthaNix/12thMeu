53a86195ebdde05f6cec77d7572cba6a2a51952a
Samantha <deathangelandcalmcrashgaming@gmail.com>
2026-06-06 21:13:40 +0200

    fixed minigun troop gunner position missing fixed customhelmets not saving

--- addons/12th_custom_helms/config_macros.hpp
+++ addons/12th_custom_helms/config_macros.hpp
@@ -132,6 +132,7 @@ class twelfth_helmECH43A_std_##SUFFIX##_clsd: twelfth_helmECH43A_base { \
 		author = "$STR_TCP_Data_Author";\
 		dlc = "TCP";\
 		scope = 0;\
+    ace_arsenal_uniqueBase = QOUTE(twelfth_helmCH43A_std_##SUFFIX##); \
 		model = "\TCP\Characters\BLUFOR\UNSC\Army\Headgear\helmet_CH43A\h_helmet_CH43A_ChinstrapOffset.p3d";\
 		class ItemInfo: ItemInfo\
 		{\
