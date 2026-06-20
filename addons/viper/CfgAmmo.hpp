4a8e3d7996f4688ac21500513133cbf9f1ca1efa
Doruaraebe <dorugaming@outlook.com>
2026-06-20 18:39:14 +0100

    Viper Ammo Definitions

--- addons/viper/CfgAmmo.hpp
+++ addons/viper/CfgAmmo.hpp
@@ -1,230 +1,83 @@
 class CfgAmmo
 {
-	class BulletBase;
-	class SMT_200mm_HE_Slow: BulletBase //Indirect fire HE
+	class Sh_125mm_HE;
+	class M_Titan_AT;
+	class Sh_75mm_Railgun_APFSDS;
+	class SMT_200mm_HE_Slow: Sh_125mm_HE //Indirect fire HE
 	{
-		hit = 300;indirectHit = 0;indirectHitRange = 0; 
-		cartridge = "FxCartridge_65_caseless"; /// seems like standard 6.5mm round
-		visibleFire = 5; /// how big does the shooter seem to be for AI to take notice after he shoots
-		audibleFire = 8; /// how big does the shooter seem to be for AI to take notice after he shoots
-		cost = 100; 
-		typicalSpeed = 2000;
-		caliber = 2.0; /// caliber 1 equals 6.5mm round
-		model = "\A3\Weapons_f\Data\bullettracer\tracer_red"; /// just for tracer
-		tracerScale = 10; /// how big is the flying tracer
-		tracerStartTime = 0.05; // seconds
-		tracerEndTime = 1; // seconds
-		airFriction = 0;
-	    explosionEffects = "GrenadeExplosion";
-		/* z:
-			air friction value was set carefully to match RL ballistic table with high fidelity
-			source: http://www.shootingtimes.com/wp-content/blogs.dir/11/files/6-5mm-grendel-the-round-the-military-ought-to-have/6-5mm-grendel_009.jpg
-		*/
-		
-		dangerRadiusBulletClose 		= 8;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
-		dangerRadiusHit 				= 12;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
-		suppressionRadiusBulletClose 	= 6;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
-		suppressionRadiusHit 			= 8;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
-		shootDistraction 				= 10;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
+		hit = 660;indirectHit = 160;indirectHitRange = 10; 
+		typicalSpeed = 420;
+		caliber = 16; /// caliber 1 equals 6.5mm round
+
+		dangerRadiusBulletClose 		= -1;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
+		dangerRadiusHit 				= 200;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
+		suppressionRadiusBulletClose 	= -1;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
+		suppressionRadiusHit 			= 46;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
+		shootDistraction 				= -1;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
 		
 		/////////////////////ACE STUFF//////////////////////////////
- 		ACE_caliber = 13;
-		ACE_bulletLength = 70.012;
-		ACE_bulletMass = 60.0176;
-        ACE_ammoTempMuzzleVelocityShifts[]={-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[]={1.01};
-        ACE_velocityBoundaries[]={};
-        ACE_standardAtmosphere="ASM";
-        ACE_dragModel=1;
-        ACE_muzzleVelocities[]={2000};
-        ACE_barrelLengths[]={736.6}; 
-		ACE_damageType ="bullet";
+		ace_frag_charge = 4000;
+		ace_rearm_caliber = 200;
 		/////////////////////ACE STUFF//////////////////////////////
-		class CamShakeFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
-		class CamShakePlayerFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
-		
+
 	};
-    class SMT_200mm_HE: BulletBase //Fast High explosive
+    class SMT_200mm_HE: SMT_200mm_HE_Slow //Fast High explosive
 	{
-		hit = 300;indirectHit = 0;indirectHitRange = 0; 
-		cartridge = "FxCartridge_65_caseless"; /// seems like standard 6.5mm round
-		visibleFire = 5; /// how big does the shooter seem to be for AI to take notice after he shoots
-		audibleFire = 8; /// how big does the shooter seem to be for AI to take notice after he shoots
-		cost = 100; 
-		typicalSpeed = 2000;
-		caliber = 2.0; /// caliber 1 equals 6.5mm round
-		model = "\A3\Weapons_f\Data\bullettracer\tracer_red"; /// just for tracer
-		tracerScale = 10; /// how big is the flying tracer
-		tracerStartTime = 0.05; // seconds
-		tracerEndTime = 1; // seconds
-		airFriction = 0;
-	    explosionEffects = "GrenadeExplosion";
-		/* z:
-			air friction value was set carefully to match RL ballistic table with high fidelity
-			source: http://www.shootingtimes.com/wp-content/blogs.dir/11/files/6-5mm-grendel-the-round-the-military-ought-to-have/6-5mm-grendel_009.jpg
-		*/
-		
-		dangerRadiusBulletClose 		= 8;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
-		dangerRadiusHit 				= 12;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
-		suppressionRadiusBulletClose 	= 6;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
-		suppressionRadiusHit 			= 8;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
-		shootDistraction 				= 10;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
+		typicalSpeed = 1400;
 		
 		/////////////////////ACE STUFF//////////////////////////////
- 		ACE_caliber = 13;
-		ACE_bulletLength = 70.012;
-		ACE_bulletMass = 60.0176;
-        ACE_ammoTempMuzzleVelocityShifts[]={-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[]={1.01};
-        ACE_velocityBoundaries[]={};
-        ACE_standardAtmosphere="ASM";
-        ACE_dragModel=1;
-        ACE_muzzleVelocities[]={2000};
-        ACE_barrelLengths[]={736.6}; 
-		ACE_damageType ="bullet";
 		/////////////////////ACE STUFF//////////////////////////////
-		class CamShakeFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
-		class CamShakePlayerFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
-		
 	};
-    class SMT_200mm_SALH: BulletBase //Laser guided Missile
+    class SMT_200mm_SALH: M_Titan_AT //Laser guided Missile
 	{
-        laserLock=1;
-        CraterEffects = "ExploAmmoCrater";
-        fuseDistance= 10;
-        manueuvrability = 30;
-        maxSpeed = 300;
-        explosionEffects = "ExploAmmoExplosion";
+        airLock = 0;
+		autoSeekTarget = 1;
+		cmImmunity = 1;
+		flightProfiles[] = {"LOALDistance"};
+		irLock = 0;
+		laserLock=1;
+		lockSeekRadius = 3000;
+		lockType = 0;
+		manualControl = 0;
+		maxControlRange = 10000;
+		missileKeepLockedCone = 360;
+		missileLockCone = 300;
+		nvlock = 0;
+		trackLead = 0.5;
+		trackOversteer = 1;
+		weaponLockSystem = "4 + 16";
 
         
-		hit = 300;indirectHit = 0;indirectHitRange = 0; 
-		cartridge = "FxCartridge_65_caseless"; /// seems like standard 6.5mm round
-		visibleFire = 5; /// how big does the shooter seem to be for AI to take notice after he shoots
-		audibleFire = 8; /// how big does the shooter seem to be for AI to take notice after he shoots
-		cost = 100; 
-		typicalSpeed = 2000;
-		caliber = 2.0; /// caliber 1 equals 6.5mm round
-		model = "\A3\Weapons_f\Data\bullettracer\tracer_red"; /// just for tracer
-		tracerScale = 10; /// how big is the flying tracer
-		tracerStartTime = 0.05; // seconds
-		tracerEndTime = 1; // seconds
-		airFriction = 0;
-	    
-		/* z:
-			air friction value was set carefully to match RL ballistic table with high fidelity
-			source: http://www.shootingtimes.com/wp-content/blogs.dir/11/files/6-5mm-grendel-the-round-the-military-ought-to-have/6-5mm-grendel_009.jpg
-		*/
-		
-		dangerRadiusBulletClose 		= 8;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
-		dangerRadiusHit 				= 12;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
-		suppressionRadiusBulletClose 	= 6;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
-		suppressionRadiusHit 			= 8;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
-		shootDistraction 				= 10;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
+		hit = 300;indirectHit = 50;indirectHitRange = 6; 
+		typicalSpeed = 200;
+		maxSpeed = 190;
+		caliber = 1.5; /// caliber 1 equals 6.5mm round
+
+		dangerRadiusBulletClose 		= -1;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
+		dangerRadiusHit 				= -1;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
+		suppressionRadiusBulletClose 	= -1;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
+		suppressionRadiusHit 			= 40;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
+		shootDistraction 				= -1;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
 		
 		/////////////////////ACE STUFF//////////////////////////////
- 		ACE_caliber = 13;
-		ACE_bulletLength = 70.012;
-		ACE_bulletMass = 60.0176;
-        ACE_ammoTempMuzzleVelocityShifts[]={-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[]={1.01};
-        ACE_velocityBoundaries[]={};
-        ACE_standardAtmosphere="ASM";
-        ACE_dragModel=1;
-        ACE_muzzleVelocities[]={2000};
-        ACE_barrelLengths[]={736.6}; 
-		ACE_damageType ="bullet";
+		ace_rearm_caliber = 200;
 		/////////////////////ACE STUFF//////////////////////////////
-		class CamShakeFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
-		class CamShakePlayerFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
+
 	};
-    class SMT_200mm_APFSDS: BulletBase //Armour piercing
+    class SMT_200mm_APFSDS: Sh_75mm_Railgun_APFSDS //Armour piercing
 	{
-		hit = 300;indirectHit = 0;indirectHitRange = 0; 
-		cartridge = "FxCartridge_65_caseless"; /// seems like standard 6.5mm round
-		visibleFire = 5; /// how big does the shooter seem to be for AI to take notice after he shoots
-		audibleFire = 8; /// how big does the shooter seem to be for AI to take notice after he shoots
-		cost = 100; 
-		typicalSpeed = 2000;
-		caliber = 2.0; /// caliber 1 equals 6.5mm round
-		model = "\A3\Weapons_f\Data\bullettracer\tracer_red"; /// just for tracer
-		tracerScale = 10; /// how big is the flying tracer
-		tracerStartTime = 0.05; // seconds
-		tracerEndTime = 1; // seconds
-		airFriction = 0;
-	    explosionEffects = "GrenadeExplosion";
-		/* z:
-			air friction value was set carefully to match RL ballistic table with high fidelity
-			source: http://www.shootingtimes.com/wp-content/blogs.dir/11/files/6-5mm-grendel-the-round-the-military-ought-to-have/6-5mm-grendel_009.jpg
-		*/
-		
-		dangerRadiusBulletClose 		= 8;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
-		dangerRadiusHit 				= 12;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
-		suppressionRadiusBulletClose 	= 6;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
-		suppressionRadiusHit 			= 8;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
-		shootDistraction 				= 10;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
+		hit = 1200;indirectHit = 0;indirectHitRange = 0; 
+		typicalSpeed = 3500;
+		caliber = 35; /// caliber 1 equals 6.5mm round
+
+		dangerRadiusBulletClose 		= 20;	/// defines how far the AI gets alerted by the projectile's impact or explosion. Default value -1 makes this distance be automatically derived from the hit and indirectHit properties.
+		dangerRadiusHit 				= 60;	/// defines how far the AI gets alerted by the bullet's pass. Default value -1 disables the detection. (works only for bullets, no other projectiles)
+		suppressionRadiusBulletClose 	= 12;	/// defines the max. distance at which the AI becomes suppressed by the projectile's impact or explosion. Default value -1 disables the suppressive effect
+		suppressionRadiusHit 			= 16;	/// defines the max. distance at which the AI becomes suppressed by the bullet's pass. Default value -1 disables the suppressive effect (works only for bullets, no other projectiles)
+		shootDistraction 				= -1;	/// -1 by default to be computed from visibleFire and audibleFire, describes how much does the shoot distract the shooter
 		
 		/////////////////////ACE STUFF//////////////////////////////
- 		ACE_caliber = 13;
-		ACE_bulletLength = 70.012;
-		ACE_bulletMass = 60.0176;
-        ACE_ammoTempMuzzleVelocityShifts[]={-26.55, -25.47, -22.85, -20.12, -16.98, -12.80, -7.64, -1.53, 5.96, 15.17, 26.19};
-        ACE_ballisticCoefficients[]={1.01};
-        ACE_velocityBoundaries[]={};
-        ACE_standardAtmosphere="ASM";
-        ACE_dragModel=1;
-        ACE_muzzleVelocities[]={2000};
-        ACE_barrelLengths[]={736.6}; 
-		ACE_damageType ="bullet";
+		ace_rearm_caliber = 200;
 		/////////////////////ACE STUFF//////////////////////////////
-		class CamShakeFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
-		class CamShakePlayerFire /// doesn't cause any camera shake
-		{
-			power = 0;
-			duration = 0;
-			frequency = 0;
-			distance = 0;
-		};
 	};
 };
